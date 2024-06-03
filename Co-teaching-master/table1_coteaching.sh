11d16a5ae10fedba90a0a58fe321232d2355f5b5
bae301764 <bae301764@naver.com>
2024-06-03 23:13:26 +0900

    commit

--- Co-teaching-master/table1_coteaching.sh
+++ Co-teaching-master/table1_coteaching.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for dataset in cifar10; do
-    for noise_type in instance; do
+    for noise_type in symmetric pairflip instance; do
         for seed in 1 2 3 4 5; do
             for noise_rate in 0.2 0.4; do
                     # Run Python script with specific parameters and append results to the log file
